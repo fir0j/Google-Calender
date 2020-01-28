b1ff415f6f4f828de90704094cd64ccd364590d1
fir0j <firoj.is.availble@gmail.com>
2020-01-28 16:43:18 +0530

    solved conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-	return <div className="text-green-500">Hello World</div>;
+	return (
+		<div className="text-green-500">
+			<p>Hello World</p>
+		</div>
+	);
 }
 
 export default App;
