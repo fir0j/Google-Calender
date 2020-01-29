c4c78f4c9511703c7f5333a49e2737889837a158
fir0j <firoj.is.availble@gmail.com>
2020-01-29 18:16:00 +0530

    added create button

--- src/components/Sidebar.component.js
+++ src/components/Sidebar.component.js
@@ -1,10 +1,20 @@
 import React from 'react';
 export const Sidebar = () => {
 	return (
-		<div className="w-full border">
-			<div>
-				<button>Create</button>
+		<div className="w-full border text-center">
+			<div className="py-2 ml-2 mt-3 mb-3 flex justify-center rounded-full text-xs items-center cursor-pointer shadow-full">
+				<div>
+					<svg className="w-6 h-6" viewBox="0 0 36 36">
+						<path fill="#34A853" d="M16 16v14h4V20z" />
+						<path fill="#4285F4" d="M30 16H20l-4 4h14z" />
+						<path fill="#FBBC05" d="M6 16v4h10l4-4z" />
+						<path fill="#EA4335" d="M20 16V6h-4v14z" />
+						<path fill="none" d="M0 0h36v36H0z" />
+					</svg>
+				</div>
+				<div className="ml-1 pr-2">Create</div>
 			</div>
+
 			<div>Calender</div>
 			<div>
 				<input className="w-full border" type="text" />
