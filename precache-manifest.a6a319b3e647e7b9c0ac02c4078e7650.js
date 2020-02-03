1ef38d185a24fba2b0b5a5762f8e3febb8019356
fir0j <firoj.is.availble@gmail.com>
2020-02-03 12:42:16 +0530

    Updates

---
+++ precache-manifest.a6a319b3e647e7b9c0ac02c4078e7650.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9f95b0986314a4d62aa5ce278d8a26",
+    "url": "/Google-Calender/index.html"
+  },
+  {
+    "revision": "c38c570ffc3785c2ef77",
+    "url": "/Google-Calender/static/css/2.6d986cef.chunk.css"
+  },
+  {
+    "revision": "9180ee6c6e16b55e212b",
+    "url": "/Google-Calender/static/css/main.65b5c140.chunk.css"
+  },
+  {
+    "revision": "c38c570ffc3785c2ef77",
+    "url": "/Google-Calender/static/js/2.1ca404a9.chunk.js"
+  },
+  {
+    "revision": "56633a82a10171c1468af438e6dc7bdd",
+    "url": "/Google-Calender/static/js/2.1ca404a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9180ee6c6e16b55e212b",
+    "url": "/Google-Calender/static/js/main.69e3fb5c.chunk.js"
+  },
+  {
+    "revision": "0fa6dac22a81e9d7cf32",
+    "url": "/Google-Calender/static/js/runtime-main.ff22ce50.js"
+  },
+  {
+    "revision": "41e4ce9d6c42e42a9d1dbffd1f2d5ac8",
+    "url": "/Google-Calender/static/media/next.41e4ce9d.svg"
+  },
+  {
+    "revision": "d413bfac509399f658fbc6f4d3e39939",
+    "url": "/Google-Calender/static/media/previous.d413bfac.svg"
+  }
+]);
